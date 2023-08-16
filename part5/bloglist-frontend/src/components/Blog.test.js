fdf005f2486f38b0e9941dc1024af542acf27b7f
Ratiu Gabriel Raul <ratiugabrielraul@gmail.com>
2023-08-16 09:59:24 +0300

    5.14 - tested if url and likes are shown when show btn is pressed

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Blog from "./Blog";
 
-test('render title and author, but not URL or likes by default', () => {
+describe("Blog tests block", () => {
     const blog = {
         title: "Living life",
         author: "John Doe",
@@ -14,20 +14,41 @@ test('render title and author, but not URL or likes by default', () => {
             password: "password"
         }
     };
-    const component = render(<Blog key={blog.id} blog={blog} />)
 
-    const titleElement = component.container.querySelector(".title");
-    expect(titleElement).toHaveTextContent(blog.title);
+    let component;
 
-    const authorElement = component.container.querySelector(".author");
-    expect(authorElement).toHaveTextContent(blog.author);
-
-    // expect(component.queryByText(blog.url)).not.toBeInTheDocument();
-
-    const urlElement = component.container.querySelector('.url');
-    expect(urlElement).toBeNull();
-
-    expect(component.queryByText(blog.likes)).not.toBeInTheDocument();
+    beforeEach(() => {
+        component = render(<Blog key={blog.id} blog={blog} />)
+    })
 
 
-});
+
+    test("render title and author, but not URL or likes by default", () => {
+
+
+        const titleElement = component.container.querySelector(".title");
+        expect(titleElement).toHaveTextContent(blog.title);
+
+        const authorElement = component.container.querySelector(".author");
+        expect(authorElement).toHaveTextContent(blog.author);
+
+        // expect(component.queryByText(blog.url)).not.toBeInTheDocument();
+
+        const urlElement = component.container.querySelector('.url');
+        expect(urlElement).toBeNull();
+
+        expect(component.queryByText(blog.likes)).not.toBeInTheDocument();
+
+
+    }),
+
+        test("check if url and likes are shown when button is clicked", () => {
+
+            const button = component.container.querySelector(".hide-show-btn");
+            fireEvent.click(button)
+
+            const blogDetails = component.container.querySelector(".blog-details");
+            expect(blogDetails).toBeInTheDocument();
+        })
+    // to continue 
+})
