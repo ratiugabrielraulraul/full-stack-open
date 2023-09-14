0fefef55f3e168a2ad12d155b699b45cdae315cc
Ratiu Gabriel Raul <ratiugabrielraul@gmail.com>
2023-09-14 10:04:19 +0300

    Implement retrieving anecdotes from the server using React Query-6.20.

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,57 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query';
+import { getAnecdotes } from './request'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+
+  if(result.isLoading){
+    return <div>Loading data...</div>
+  }
+  if(result.isError){
+    return <div>Anecdote service is not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/request.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+
+export const getAnecdotes = async () => {
+    const response = await axios.get('http://localhost:3001/anecdotes');
+    return response.data;
+  };
